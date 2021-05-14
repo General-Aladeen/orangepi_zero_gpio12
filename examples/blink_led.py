c3a706333dbebfd474fb42d458d44d49870fd2c0
General-Aladeen <83897925+General-Aladeen@users.noreply.github.com>
2021-05-14 12:16:27 +0530

    Update blink_led.py

--- examples/blink_led.py
+++ examples/blink_led.py
@@ -24,7 +24,7 @@ __maintainer__ = __author__
 __email__ = "support@olimex.com"
 
 
-led = port.PH12 #Updated mapping 
+led = port.PH03 #Updated mapping 
 
 gpio.init()
 gpio.setcfg(led, gpio.OUTPUT)
