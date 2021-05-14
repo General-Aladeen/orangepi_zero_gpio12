fda35f46e15620fd27d24f10733c0cd11fc7f23d
General-Aladeen <83897925+General-Aladeen@users.noreply.github.com>
2021-05-14 14:13:30 +0530

    Update AJ.py

--- examples/AJ.py
+++ examples/AJ.py
@@ -7,11 +7,14 @@ gpio.init ()
 
 
 led = port.PA3
+led1 = port.PA2
 gpio.setcfg (led, gpio.OUTPUT)
-
+gpio.setcfg (led1, gpio.OUTPUT)
 # set the status of the leds (high / low)
 while True:
         gpio.output (led, gpio.HIGH)
+        gpio.output (led1, gpio.HIGH)
         sleep (1)
         gpio.output (led, gpio.LOW)
+        gpio.output (led1, gpio.LOW)
         sleep (1)
