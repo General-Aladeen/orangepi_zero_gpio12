410ba7ed0b5e4126a21c63a2b2c692f7ba3df0b6
General-Aladeen <83897925+General-Aladeen@users.noreply.github.com>
2021-05-14 14:35:40 +0530

    Update AJ.py

--- examples/AJ.py
+++ examples/AJ.py
@@ -6,10 +6,10 @@ from pyA20.gpio import port
 gpio.init ()
 
 
-led1 = port.PA2
-led2 = port.PA3
-led3 = port.PA14
-led4 = port.PA6
+led1 = port.PA1
+led2 = port.PA10
+led3 = port.PA12
+led4 = port.PA15
 
 gpio.setcfg (led1, gpio.OUTPUT)
 gpio.setcfg (led2, gpio.OUTPUT)
