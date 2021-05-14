550cb1289817043f69f9b26636360158a5dc4137
General-Aladeen <83897925+General-Aladeen@users.noreply.github.com>
2021-05-14 17:09:07 +0530

    Update AJ.py

--- examples/AJ.py
+++ examples/AJ.py
@@ -5,36 +5,65 @@ from pyA20.gpio import port
 
 gpio.init ()
 
+#LED 1 (TOP RIGHT)
+led1R=port.PA0
+led1G=port.PA1
+led1B=port.PA3
+#LED 2 (TOP RIGHT)
+led2R = port.PA13
+led2G = port.PA10
+led2B = port.PA2
+#LED 3 (BOTTOM LEFT)
+led3R = port.PA11
+led3G = port.PA12
+led3B = port.PA6
+#LED 4 (BOTTOM RIGHT)
+led4R = port.PA16
+led4R = port.PA15
+led4R = port.PA14
 
-led1 = port.PA1
-led2 = port.PA10
-led3 = port.PA12
-led4 = port.PA15
 
-gpio.setcfg (led1, gpio.OUTPUT)
-gpio.setcfg (led2, gpio.OUTPUT)
-gpio.setcfg (led3, gpio.OUTPUT)
-gpio.setcfg (led4, gpio.OUTPUT)
-# set the status of the leds (high / low)
-while True:
-        gpio.output (led1, gpio.LOW)
-        gpio.output (led2, gpio.LOW)
-        gpio.output (led3, gpio.LOW)
-        gpio.output (led4, gpio.LOW)
+#----------Init----------#
+gpio.setcfg (led1R, gpio.OUTPUT)
+gpio.setcfg (led1G, gpio.OUTPUT)
+gpio.setcfg (led1B, gpio.OUTPUT)
+
+gpio.setcfg (led2R, gpio.OUTPUT)
+gpio.setcfg (led2G, gpio.OUTPUT)
+gpio.setcfg (led2B, gpio.OUTPUT)
+
+gpio.setcfg (led3R, gpio.OUTPUT)
+gpio.setcfg (led3G, gpio.OUTPUT)
+gpio.setcfg (led3B, gpio.OUTPUT)
+
+gpio.setcfg (led4R, gpio.OUTPUT)
+gpio.setcfg (led4G, gpio.OUTPUT)
+gpio.setcfg (led4B, gpio.OUTPUT)
+
+i = 0
+
+# set the status of the leds (high / low):
+while i != 5:
+        gpio.output (led1R, gpio.LOW)
+        gpio.output (led2R, gpio.LOW)
+        gpio.output (led3R, gpio.LOW)
+        gpio.output (led4R, gpio.LOW)
         sleep (0.1)
-        gpio.output (led1, gpio.HIGH)
-        gpio.output (led2, gpio.HIGH)
-        gpio.output (led3, gpio.HIGH)
-        gpio.output (led4, gpio.HIGH)
+        gpio.output (led1R, gpio.HIGH)
+        gpio.output (led2R, gpio.HIGH)
+        gpio.output (led3R, gpio.HIGH)
+        gpio.output (led4R, gpio.HIGH)
         sleep (0.1)
-        gpio.output (led1, gpio.LOW)
-        gpio.output (led2, gpio.LOW)
-        gpio.output (led3, gpio.LOW)
-        gpio.output (led4, gpio.LOW)
+        gpio.output (led1R, gpio.LOW)
+        gpio.output (led2R, gpio.LOW)
+        gpio.output (led3R, gpio.LOW)
+        gpio.output (led4R, gpio.LOW)
         sleep (0.1)
-        gpio.output (led1, gpio.HIGH)
-        gpio.output (led2, gpio.HIGH)
-        gpio.output (led3, gpio.HIGH)
-        gpio.output (led4, gpio.HIGH)
+        gpio.output (led1R, gpio.HIGH)
+        gpio.output (led2R, gpio.HIGH)
+        gpio.output (led3R, gpio.HIGH)
+        gpio.output (led4R, gpio.HIGH)
         sleep (1)
+        i = i+1
+        
         
